1e699022260b589888323118d3c574ea11f0cd04
PICTORY-droid <oceanlumes@gmail.com>
2026-06-09 19:47:42 +0900

    feat: show register work info section

--- app/(public)/register/page.tsx
+++ app/(public)/register/page.tsx
@@ -1,6 +1,7 @@
 import { RegisterBasicInfoSection } from "./_components/RegisterBasicInfoSection";
 import { RegisterIntroCard } from "./_components/RegisterIntroCard";
 import { RegisterPageHeader } from "./_components/RegisterPageHeader";
+import { RegisterWorkInfoSection } from "./_components/RegisterWorkInfoSection";
 
 export default function RegisterPage() {
   return (
@@ -9,6 +10,7 @@ export default function RegisterPage() {
         <RegisterPageHeader />
         <RegisterIntroCard />
         <RegisterBasicInfoSection />
+        <RegisterWorkInfoSection />
       </div>
     </main>
   );
